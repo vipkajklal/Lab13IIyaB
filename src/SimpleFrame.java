a8b71ea781739afd0a41879adf1e81f6ff3f650c
Airis <vipkajklal@gmail.com>
2021-06-14 03:00:46 +0900

    Initial commit

---
+++ src/SimpleFrame.java
@@ -0,0 +1,18 @@
+import java.awt.*;
+import java.awt.event.*;
+
+public class SimpleFrame extends Frame {
+    SimpleFrame(String s){
+        super(s);
+        setSize(400,150);
+        setVisible(true);
+        addWindowListener(new WindowAdapter(){
+            public void windowClosing(WindowEvent ev){
+                System.exit(0);
+            }
+        });
+    }
+    public static void main(String[] args){
+        new SimpleFrame("Моя программа");
+    }
+}
